c1eca78c1127a38f3043588ca3446a7bce429075
Brian OConnor <boconnor20@gmail.com>
2021-09-22 21:07:49 -0500

    Navbar additions

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "../logo.png";
+
+const Navbar = () => {
+    return (
+ <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <div className="container-fluid">
+        <a className="navbar-brand" href="#"><img className= "logo" src={logo} alt="logo"/></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+        aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav m-auto">
+                <li className="nav-item active">
+                 <a className="nav-link" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About Me</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Portfolio</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Contacts</a>
+                </li>
+            </ul>
+          
+        </div>
+    </div>
+ </nav>
+
+    )
+}
+
+export default Navbar
